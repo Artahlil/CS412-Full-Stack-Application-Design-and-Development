7f2ab66f4def9fb7db7348e097384580dec84edb
Abdulrahim Tahlil <artahlil@bu.edu>
2020-11-21 16:20:33 -0500

    ps5b

--- routes/ps4.js
+++ routes/ps4.js
@@ -2,18 +2,50 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 const FETCHCONFIG = require('../config/fetchConfig');
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const asyncGet = promisify(client.get).bind(client)
+const asyncSet = promisify(client.set).bind(client)
+const asyncExists = promisify(client.exists).bind(client)
+const asyncExpire = promisify(client.expire).bind(client)
+
+client.flushdb((error, response) => {
+    if (error) {
+        throw new Error("Error while flushing")
+    }
+});
+    router.post('/', async (req, res, next) => {
+        const locale = req.body.cityname;
+        let match = await asyncExists(locale);
+        if (match) {
+            let cached_data = await asyncGet(locale);
+            let cached_obejct = JSON.parse(cached_data);
+            const temp = cached_obejct.temp_f;
+            let response = {
+                data: cached_obejct,
+                cached: true
+            }
+                res.send(response);
+     }
+        else {
+            let raw = await fetch(FETCHCONFIG.fetchOptions.url+locale.toString());
+            let clean = await raw.json();
+            await asyncSet(locale, JSON.stringify(clean));
+            let response = {
+                data: clean,
+                cached: false
+        }
+            await asyncExpire(locale, 15);
+            res.send(response);
+      }
+    })
 
 const response = async locale => {
     let raw = await fetch(FETCHCONFIG.fetchOptions.url+locale.toString());
     let clean = await raw.json();
     return clean;
 }
-
-router.route('/')
-    .post( (req, res, next) => {
-        response(req.body.cityname)
-            .then(clean => res.render('ps4', {'city' : clean.name, 'temp' : clean.temp_f }))
-            .catch(error => console.log("Failed to fetch JSON"))
-    })
 
 module.exports = router;
