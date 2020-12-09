0259fb3d170253a0eac2b3d08cd8e8b7d28f692f
Abdulrahim Tahlil <artahlil@bu.edu>
2020-12-08 20:18:03 -0500

    ps6

--- PS6/PS6/src/app/app.component.ts
+++ PS6/PS6/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { mock } from './Mock';
 import { Data } from './Data';
-
+import { fetchOptions } from '/fetchConfig'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,12 +11,25 @@ export class AppComponent {
   title = 'PS6';
   mock = Data[] = mock;
   data = Data;
-  selectedCity = Data;
+  selectedCity = string;
   fetchData = false;
   all = false;
+  clicked: boolean = false;
 
-  constructor() { }
-  
+  constructor() { 
+    this.getInfo();
+  }
+  getInfo():void {
+    for (const city of Mock) {
+      const newCity: Data = {
+        city: city.city,
+        temp_f: city.temp_f,
+      };
+      this.mock.push(newCity);
+    } 
+    this.selectedCity = null;
+  }
+ 
   raw(): void {
     this.all = !this.all;
   }
@@ -28,6 +41,26 @@ export class AppComponent {
   setSelectedCity(data = Data;): void {
     this.selectedCity = data;
     console.log(`${data.city}`);
+  }
+  
+  clicked() {
+    const url = fetchOptions.url+this.selectedCity.toString()
+    const res = Mock[url];
+    if (res.length >= 1) {
+      const data = res[res.length - 1];
+      this.selectedCity = {
+        city: data.city,
+        temp_f: data.temp_f,
+        temp_c: data.temp_c,
+        feelslike_c: data.feelslike_c,
+        feelslike_f: data.feelslike_f
+      };
+    } else {
+      this.selectedCity = {
+        console.log("empty")
+      };
+    }
+    this.clicked = true;
   }
   convert(file: Data): string {
     return JSON.stringify(file, null, 2);;
