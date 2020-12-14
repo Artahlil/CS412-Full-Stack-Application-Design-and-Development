97be74e627b3f5fbf3625985f1c96ab933babf87
Abdulrahim Tahlil <artahlil@bu.edu>
2020-12-14 11:38:27 -0500

    ps7

--- PS7/PS6/src/app/Weather-Mock.ts
+++ PS7/PS6/src/app/Weather-Mock.ts
@@ -14,5 +14,19 @@ export const Weather_reports: Weather[] =
       wxDesc: 'Partly sunny',
       tempC: 22.0,
       tempF: 71.6
+    },
+    {
+      lat: 12.5,
+      lon: 0.10,
+      wxDesc: 'Partly sunny',
+      tempC: 14.0,
+      tempF: 57.6
+    },
+    {
+      lat: 42.3,
+      lon: 71.0,
+      wxDesc: 'Mostly cloudy',
+      tempC: 2.7,
+      tempF: 37
     }
   ];
