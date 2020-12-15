69825cd20c7867cbbb111291cc9d3627ed471a13
Abdulrahim Tahlil <artahlil@bu.edu>
2020-12-15 15:21:32 -0500

    ps7

--- PS7/PS6/src/app/services/weather.service.ts
+++ PS7/PS6/src/app/services/weather.service.ts
@@ -8,7 +8,7 @@ import {weatherConfigs as config} from '../config/weatherConfigs';
 })
 export class WeatherService {
   getWeather(lat, lon) {
-    let url = `http://api.weatherunlocked.com/api/current/${lat},${lon}?app_id=${config.app_id}app_key=${config.app_key}`;
+    let url = 'http://api.weatherunlocked.com/api/current/${lat},${lon}?app_id=${config.app_id}app_key=${config.app_key}';
     return this.http.get(url);
   }
 
